2bab8600c3e062f7e272215a30c68042b1bd86f1
Syed <syedarshadmehdi@gmail.com>
2021-08-26 19:06:19 +1000

    First Commit , Finish the Application Form with Validation. Working on Multiple application ID TYPE SELECTION.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppFormComponent } from './app-form/app-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MultipleRadiobuttonsComponent } from './multiple-radiobuttons/multiple-radiobuttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    AppFormComponent,
+    MultipleRadiobuttonsComponent
+    
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
